907533ddf48079088f6666fc0dcc451652d2eb1f
Hakan <hakanyalitekin@gmail.com>
2020-01-31 13:09:52 +0300

    Yer düzenlemesi

--- AdoNetDemo/Form1.cs
+++ AdoNetDemo/Form1.cs
@@ -22,7 +22,10 @@ namespace AdoNetDemo
         {
             LoadProducts();
         }
-
+        public void LoadProducts()
+        {
+            dgvProducts.DataSource = _productDal.GetAll();
+        }
         private void btnAdd_Click(object sender, EventArgs e)
         {
             _productDal.Add(new Product
@@ -35,19 +38,6 @@ namespace AdoNetDemo
             LoadProducts();
             MessageBox.Show("Product added!");
         }
-        public void LoadProducts()
-        {
-            dgvProducts.DataSource = _productDal.GetAll();
-        }
-
-
-        private void dgvProducts_CellClick(object sender, DataGridViewCellEventArgs e)
-        {
-            txtNameUpdate.Text = dgvProducts.CurrentRow.Cells[1].Value.ToString();
-            txtUnitPriceUpdate.Text = dgvProducts.CurrentRow.Cells[2].Value.ToString();
-            txtStockAmountUpdate.Text = dgvProducts.CurrentRow.Cells[3].Value.ToString();
-        }
-
         private void btnUpdate_Click(object sender, EventArgs e)
         {
             Product product = new Product
@@ -62,13 +52,18 @@ namespace AdoNetDemo
             LoadProducts();
             MessageBox.Show("Product Updated");
         }
-
         private void btnRemove_Click(object sender, EventArgs e)
         {
             int id = Convert.ToInt32(dgvProducts.CurrentRow.Cells[0].Value);
             _productDal.Delete(id);
             LoadProducts();
             MessageBox.Show("Product Deleted!");
+        }
+        private void dgvProducts_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtNameUpdate.Text = dgvProducts.CurrentRow.Cells[1].Value.ToString();
+            txtUnitPriceUpdate.Text = dgvProducts.CurrentRow.Cells[2].Value.ToString();
+            txtStockAmountUpdate.Text = dgvProducts.CurrentRow.Cells[3].Value.ToString();
         }
     }
 }
